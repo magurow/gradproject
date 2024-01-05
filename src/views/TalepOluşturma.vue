d6032f5726bf0e604d4e6a567381f9974f789b75
eren1f <surmelisafaeren@gmail.com>
2024-01-05 23:01:11 +0300

    talep oluşturma yenilemesi

--- src/views/TalepOluşturma.vue
+++ src/views/TalepOluşturma.vue
@@ -3,11 +3,30 @@
     <div>
       <h1>Talep Oluşturma</h1>
       <form @submit.prevent="submitForm">
-        <label>
-          Talep Oluşturunuz:
-          <textarea v-model="talep" class="inputField" required></textarea>
+        <label class="labeltext">
+          Talep Türünü Seçiniz:
+          <select v-model="selectedTalep" class="selectField" required>
+            <option value="" disabled selected>Seçiniz</option>
+            <option value="kontenjan-arttirma">Kontenjan Arttırma</option>
+            <option value="ders-yuku-arttirma">Ders Yükü Arttırma</option>
+            <option value="ders-yuku-azaltma">Ders Yükü Azaltma</option>
+            <option value="sosyal-secimlik-ders-alma">Sosyal Seçimlik Ders Alma</option>
+            <option value="baska-bolumden-secimlik-ders-alma">Baska Bölümden Seçimlik Ders Alma</option>
+            <option value="baska-bolumden-ortak-ders-alma">Baska Bölümden Ortak Ders Alma</option>
+            <option value="esdeğer-ders-saydirma-talepi">Eşdeğer Ders Saydırma Talepi</option>
+            <option value="oncelikli-alinmasi-gereken-dersin-ertelenmesi">Öncelikli Alınması Gereken Dersin Ertelenmesi</option>
+            <option value="diger">Diğer</option>
+          </select>
         </label>
-        <button type="submit" class="talepButton">Gönder</button>
+
+        
+        <template v-if="selectedTalep">
+          <label>
+            Talep Oluşturunuz:
+            <textarea v-model="talep" class="inputField" required></textarea>
+          </label>
+          <button type="submit" class="talepButton">Gönder</button>
+        </template>
       </form>
     </div>
   </div>
@@ -27,14 +46,16 @@ export default {
   },
   setup() {
     const talep = ref('');
+    const selectedTalep = ref('');
 
     const submitForm = () => {
-      // Don't forget the logic here
+      // Don't forget the logic here(note to oneself)
       console.log('Form submitted with talep:', talep.value);
     };
 
     return {
       talep,
+      selectedTalep,
       submitForm,
     };
   },
@@ -52,7 +73,7 @@ export default {
   margin-top: 5%;
   margin-left: 30%;
   border-radius: 20px;
-  padding: 20px; /* Add padding for better spacing */
+  padding: 5%;
 }
 
 .inputField {
@@ -60,6 +81,13 @@ export default {
   height: 100px;
   margin-right: 85px;
   resize: none;
+  padding: 8px;
+  margin-bottom: 10px;
+}
+
+.selectField {
+  width: 80%; 
+  margin-right: 85px;
   padding: 8px;
   margin-bottom: 10px;
 }
@@ -76,6 +104,10 @@ export default {
 .talepButton:hover{
   cursor: pointer;
   background-color: #412787;
+}
+.labeltext {
+  color: black;
+
 }
 </style>
 
