9f717f48bdb728594f1a8e923591b826e8caa70f
poxju <veziroglue@gmail.com>
2024-01-10 00:14:15 +0300

    under construction:   src/views/TalepOlusturma.vue

--- src/views/TalepOlusturma.vue
+++ src/views/TalepOlusturma.vue
@@ -5,7 +5,7 @@
       <form @submit.prevent="submitForm">
         <label>
           Talep Türünü Seçiniz:
-          <select v-model="selectedTalep" class="selectField" required>
+          <select v-model="selectedTalep" class="selectField" required @change="handleRequestChange">
             <option value="" disabled selected>Seçiniz</option>
             <option value="kontenjan-arttirma">Kontenjan Arttırma</option>
             <option value="ders-yuku-arttirma">Ders Yükü Arttırma</option>
@@ -13,20 +13,45 @@
             <option value="sosyal-secimlik-ders-alma">Sosyal Seçimlik Ders Alma</option>
             <option value="baska-bolumden-secimlik-ders-alma">Baska Bölümden Seçimlik Ders Alma</option>
             <option value="baska-bolumden-ortak-ders-alma">Baska Bölümden Ortak Ders Alma</option>
-            <option value="esdeğer-ders-saydirma-talepi">Eşdeğer Ders Saydırma Talepi</option>
+            <option value="esdeğer-ders-saydirma-talebi">Eşdeğer Ders Saydırma Talebi</option>
             <option value="oncelikli-alinmasi-gereken-dersin-ertelenmesi">Öncelikli Alınması Gereken Dersin Ertelenmesi</option>
             <option value="diger">Diğer</option>
           </select>
         </label>
 
+        <label v-if="showDepartment">
+          Bölüm:
+          <select v-model="selectedDepartment" class="selectField" required>
+            <option value="" disabled selected>Seçiniz</option>
+            <option v-for="option in departmentOptions" :key="option" :value="option">{{ option }}</option>
+          </select>
+        </label>
+
+        <label v-if="showselectedDepartmentCourse">
+          Ders:
+          <select v-model="selectedCourse" class="selectField" required>
+            <option value="" disabled selected>Seçiniz</option>
+            <option v-for="option in courseOptions" :key="option" :value="option">{{ option }}</option>
+          </select>
+        </label>
+
         
-        <template v-if="selectedTalep">
-          <label>
-            Talep Oluşturunuz:
+        <label v-if="showSection">
+          Sube:
+          <select v-model="selectedSection" class="selectField" required>
+            <option value="" disabled selected>Seçiniz</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            </select>
+        </label>
+        
+        
+        <label v-if="showRequest">
+          Talep Oluşturunuz:
             <textarea v-model="talep" class="inputField" required></textarea>
           </label>
           <button type="submit" class="talepButton">Gönder</button>
-        </template>
+        
       </form>
     </div>
   </div>
@@ -35,8 +60,8 @@
   </div>
 </template>
 
+
 <script>
-import { ref } from 'vue';
 import Sidebar from '../components/SideBar.vue';
 
 export default {
@@ -44,20 +69,107 @@ export default {
   components: {
     Sidebar,
   },
-  setup() {
-    const talep = ref('');
-    const selectedTalep = ref('');
 
-    const submitForm = () => {
-      // Don't forget the logic here(note to oneself)
-      console.log('Form submitted with talep:', talep.value);
-    };
-
+  data() {
     return {
-      talep,
-      selectedTalep,
-      submitForm,
+      talep: '',
+      selectedTalep: '',
+      selectedDepartment: '',
+      selectedCourse: '',
+      showDepartment: false,
+      showselectedDepartmentCourse: false,
+      showSection: false,
+      showRequest: false,
+      departmentOptions: [],
+      courseOptions: [],
     };
+  },
+  methods: {
+    handleRequestChange() {
+      // Birinci düzey seçeneğe göre ikinci düzey seçenekleri ayarla
+      const selectedTalep = this.selectedTalep;
+
+      switch (selectedTalep) {
+        case 'kontenjan-arttirma':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ["BİL101	BİLGİSAYAR YAZILIMI", "BİL103	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", 
+          "BİL105	PROGRAMLAMA LABORATUVARI I", "BİL110	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", "ENG199	ADVANCED ENGLISH I", 
+          "FİZ103	MEKANİK LABORATUVARI", "FİZ105	GENEL FİZİK I", "MAT151	MATEMATİKSEL ANALİZ I", "TÜRK101"];
+          break;
+        case 'ders-yuku-arttirma':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ["BİL101	BİLGİSAYAR YAZILIMI", "BİL103	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", 
+          "BİL105	PROGRAMLAMA LABORATUVARI I", "BİL110	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", "ENG199	ADVANCED ENGLISH I", 
+          "FİZ103	MEKANİK LABORATUVARI", "FİZ105	GENEL FİZİK I", "MAT151	MATEMATİKSEL ANALİZ I", "TÜRK101"];
+          break;
+        case 'ders-yuku-azaltma':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ["BİL101	BİLGİSAYAR YAZILIMI", "BİL103	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", 
+          "BİL105	PROGRAMLAMA LABORATUVARI I", "BİL110	BİLGİSAYAR MÜHENDİSLİĞİNE GİRİŞ", "ENG199	ADVANCED ENGLISH I", 
+          "FİZ103	MEKANİK LABORATUVARI", "FİZ105	GENEL FİZİK I", "MAT151	MATEMATİKSEL ANALİZ I", "TÜRK101"];
+          break;
+        case 'sosyal-secimlik-ders-alma':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ['SOS321 ILETISIM', 'SOS322 ISLETME YONETIMINE GIRIS'];
+          break;
+        case 'baska-bolumden-secimlik-ders-alma':
+          this.departmentOptions = ["BILGISAYAR MUH.", "BIYOMEDIKAL MUH.", "ENDUSTRI MUH.", "ELEKTRIK-ELEKTRONIK MUH.", "MAKINE MUH."];
+          this.courseOptions = [];
+          break;
+        case 'baska-bolumden-ortak-ders-alma':
+          this.departmentOptions = ["BILGISAYAR MUH.", "BIYOMEDIKAL MUH.", "ENDUSTRI MUH.", "ELEKTRIK-ELEKTRONIK MUH.", "MAKINE MUH."];
+          break;
+        case 'esdeğer-ders-saydirma-talebi':
+          this.departmentOptions = ["BILGISAYAR MUH.", "BIYOMEDIKAL MUH.", "ENDUSTRI MUH.", "ELEKTRIK-ELEKTRONIK MUH.", "MAKINE MUH."];
+          break;
+        case 'oncelikli-alinmasi-gereken-dersin-ertelenmesi':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ['Option 8.1.1', 'Option 8.1.2', 'Option 8.1.3'];
+          break;
+        case 'diger':
+          this.departmentOptions = ["BILGISAYAR MUH."];
+          this.courseOptions = ['Option 9.1.1', 'Option 9.1.2', 'Option 9.1.3'];
+          break;
+        default:
+          this.departmentOptions = [];
+          this.courseOptions = [];
+          break;
+      }
+      // İkinci düzey dropdown'u göster veya gizle
+      this.showDepartment = this.departmentOptions.length > 0;
+      this.showselectedDepartmentCourse = this.showDepartment && this.courseOptions.length > 0;
+      this.showSection = this.showselectedDepartmentCourse;
+      this.showRequest = this.showSection;
+    },
+    handleDepartmentChange() { // under construction
+      const selectedDepartment = this.selectedDepartment;
+      switch (selectedDepartment) {
+        case 'BILGISAYAR MUH.':
+
+          break;
+        case 'BIYOMEDIKAL MUH.':
+          this.courseOptions = ['Option 2.2.1', 'Option 2.2.2', 'Option 2.2.3'];
+          break;
+        case 'ENDUSTRI MUH.':
+          this.courseOptions = ['Option 3.2.1', 'Option 3.2.2', 'Option 3.2.3'];
+          break;
+        case 'ELEKTRIK-ELEKTRONIK MUH.':
+          this.courseOptions = ['Option 4.2.1', 'Option 4.2.2', 'Option 4.2.3'];
+          break;
+        case 'MAKINE MUH.':
+          this.courseOptions = ['Option 5.2.1', 'Option 5.2.2', 'Option 5.2.3'];
+          break;
+        default:
+          this.courseOptions = [];
+          break;
+      }
+      // Üçüncü düzey dropdown'u göster veya gizle
+      this.showselectedDepartmentCourse = this.showDepartment && this.courseOptions.length > 0;
+    },
+    submitForm() {
+      console.log('Form submitted with talep:', this.talep);
+    },
+   
   },
 };
 </script>
@@ -68,12 +180,12 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: #e4e4e4;
+  background: #ccc;
   width: 50%;
   margin-top: 5%;
   margin-left: 30%;
   border-radius: 20px;
-  padding: 5%;
+  padding: 2%;
 }
 
 .inputField {
